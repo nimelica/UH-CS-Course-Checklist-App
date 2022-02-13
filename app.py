79215bf1aa6088cd351c9604ad70962c1e5081d1
Nimet Ozkan <melaht2016@icloud.com>
2022-02-13 02:46:14 -0600

    added css file

--- app.py
+++ app.py
@@ -5,11 +5,39 @@ import sqlite3, random
 app = Flask(__name__)
 app.secret_key = "12ev@09_9kT;bjs8$32jhbd_sjhAdhc%mhn_773gvsahG/HSI*IH_jbs"
 
+# cookie name for sessions
+app.config['SESSION_COOKIE_NAME'] = "75jhbkdaCooKiebMONsteRRs!7)^&hbsh$4)"
 
-@app.route('/')
+# sessions in flask allows us to share data
+# between different functions 
+# w/ sessions, we're working in global scope
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    item = get_db()
-    return item[0]
+    session["all_courses"], session["checklist_courses"] = get_db()
+    return render_template('index.html', all_courses=session["all_courses"], 
+                            checklist_names=session["checklist_courses"])
+
+
+@app.route('/add_class', methods=['POST'])
+def add_class():
+    session["checklist_courses"].append(request.form["select_courses"])
+    return render_template('index.html', all_courses=session["all_courses"], 
+                            checklist_names=session["checklist_courses"])
+
+
+@app.route('/remove_class', methods=['POST'])
+def remove_class():
+    checkboxes = request.form.getlist("check")
+
+    for choice in checkboxes:
+        if choice in session["checklist_courses"]:
+            pos = session['checklist_courses'].index(choice)
+            session["checklist_courses"].pop(pos)
+            session.modified = True
+
+    return render_template('index.html', all_courses=session["all_courses"], 
+                            checklist_names=session["checklist_courses"])
 
 
 def get_db():
@@ -17,11 +45,16 @@ def get_db():
     if db is None:
         db = g._database = sqlite3.connect('course_list.db')
         cursor = db.cursor()
-        cursor.execute("select name from courses")
-        all_tuples = cursor.fetchall()
-        all_names = [str(tup[0]) for tup in all_tuples]
+        cursor.execute("select * from courses")
+        all_data = cursor.fetchall()
+        # list comprehension to get only string w/o tuples
+        all_names = [str(tup[1]) for tup in all_data]
 
-    return all_names
+        checklist = all_names.copy()
+        random.shuffle(checklist)
+        checklist = checklist[::4] # get up to 3 items as random
+    # return a tuple
+    return all_names, checklist
 
 
 @app.teardown_appcontext
