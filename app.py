ae715c99ae91c10692146ef06fd56e38b470eb14
Nimet Ozkan <melaht2016@icloud.com>
2022-02-12 22:04:13 -0600

    added template folder

--- app.py
+++ app.py
@@ -8,14 +8,20 @@ app.secret_key = "12ev@09_9kT;bjs8$32jhbd_sjhAdhc%mhn_773gvsahG/HSI*IH_jbs"
 
 @app.route('/')
 def index():
-    return "HELLO"
+    item = get_db()
+    return item[0]
 
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect('course_list.db')
-    return db
+        cursor = db.cursor()
+        cursor.execute("select name from courses")
+        all_tuples = cursor.fetchall()
+        all_names = [str(tup[0]) for tup in all_tuples]
+
+    return all_names
 
 
 @app.teardown_appcontext
